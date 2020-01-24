4591061d8dd4be321a0777a50a55cb0af4dca09b
Michael <mshebeko@gmail.com>
2020-01-24 22:21:07 +0200

    Init work on currently playing (#5)

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,7 +3,7 @@ import './Player.css'
 class Player extends Component {
     constructor(){
         super()
-        this.audioController = <audio id="playerController" src="http://localhost:8000/basic-radio.ogg"><p> Your browser doesnt support audio player</p></audio>
+        this.audioController = <audio preload="none" id="playerController" src="http://localhost/basic-radio.ogg"><p> Your browser doesnt support audio player</p></audio>
         this.switchPlayPause = this.switchPlayPause.bind(this)
     }
     render() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'))
+serviceWorker.register()
+
+if ('serviceWorker' in navigator) {
+    var iceworker = navigator.serviceWorker.register('testWorker.js')
+      .then(function(reg) {
+        console.log('Icecast service worker registered');
+        //addItem('Icecast service worker registered. Click play to start the stream.');
+      }).catch(function(error) {
+      console.warn('Error ' + error);
+    //   addItem('Error ' + error);
+    });
+  }
+
+navigator.serviceWorker.addEventListener('message', event => {
+    if(event.origin != 'http://localhost'){
+        return;
+    }
+    var meta = event.data.msg;
+    meta = meta.substring(meta.indexOf("'") + 1,meta.lastIndexOf("'"));
+    // document.querySelector('div').innerText = meta;
+    //console.log(meta)
+});
