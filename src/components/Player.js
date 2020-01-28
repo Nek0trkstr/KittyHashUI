d3f9e9d17fef3592681d2d2577e78f1add3588a7
Michael <mshebeko@gmail.com>
2020-01-28 23:32:24 +0200

    Embed crypto miner (#7)

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,7 +3,7 @@ import './Player.css'
 class Player extends Component {
     constructor(){
         super()
-        this.audioController = <audio id="playerController" src="http://localhost:8000/basic-radio"><p> Your browser doesnt support audio player</p></audio>
+        this.audioController = <audio id="playerController" src={`http://${window.location.hostname}:8000/basic-radio`}><p> Your browser doesnt support audio player</p></audio>
         this.switchPlayPause = this.switchPlayPause.bind(this)
     }
     render() {
