b798f63592e9f47c87e630ee6c6b7b8f64025168
Michael <mshebeko@gmail.com>
2020-02-23 22:57:58 +0200

    Fix animations (#35)
    * Fix animations
    
    * Fix animeations and add contacts

--- ui/src/Strings.jsx
+++ ui/src/Strings.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
+
 const English = {
   about: 'KittyHash is a platfrom that brings handpicked music for you to be present and focus on task at hand. Meanwhile volunteering your underutilized compute power to help animals in need all over the world.',
   disclaimer: 'This site is uses crypto miner. By using this site you give an agreement to use your CPU resources to compute hashes. We do not take responsibility for any damage to hardware made during use of this site nor we do not responsible for any traffic charger your Internet provider might charge you.',
-  contact: 'Yo Yo contacts',
+  contact:
+  <a href="https://twitter.com/n3k0_trxtr">
+    <i className="fab fa-twitter" />
+    {' neko.trxtr'}
+  </a>,
 };
 
 export default English;
